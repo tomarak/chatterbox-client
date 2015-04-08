747e9ae69a362204eea9e68c31bd0b8a50e87795
Anuj Tomar <ankuto@gmail.com>
2015-04-07 17:03:12 -0700

    reworking w soln

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,38 @@
 // YOUR CODE HERE:
 
 var app = {
-	url: "https://api.parse.com/1/classes/chatterbox",
-  server: "https://api.parse.com/1/classes/chatterbox"
+  server: "https://api.parse.com/1/classes/chatterbox",
+  username: "anonymous",
+  roomname: "room",
+  friends: {},
+
+  init: function(){
+    app.username = window.location.search.substr(10);
+
+    app.$main = $(".main");
+    app.$chats = $("#chats");
+    app.$message = $(".message");
+    app.$roomSelect = $("#roomSelect");
+    app.$send = $(".send");
+
+    app.$main.on("click", ".username", app.addFriend);
+    app.$send.on("submit", app.handleSubmit)
+    setTimeout(app.fetch, 1000);
+  }
 };
 
-app.init = function(){
-  app.fetch();
 
-}
 
 app.send = function(message){
 	$.ajax({
 		type: "POST",
-		url: app.url,
+		url: app.server,
 		data: JSON.stringify(message),
 		contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      console.log(data)
+      app.fetch();
     }
 	})
 }
@@ -25,11 +40,16 @@ app.send = function(message){
 app.fetch = function(){
   $.ajax({
     type: "GET",
-    url: app.url,
+    url: app.server,
     contentType: 'application/json',
+    data: {
+      limit: 200,
+      order: "-createdAt"
+    },
     success: function (data) {
       console.log('retrieved');
       console.log(data);
+
       _.each(data.results, function(obj){
           app.addMessage(obj);
       })
@@ -38,17 +58,15 @@ app.fetch = function(){
 };
 
 app.clearMessages = function(){
-  $("#chats").text("");
+  app.$chats.html('');
 }
 
 app.addMessage = function(message){
-  
   $("#chats").append('<div class="chat">' +
-    '<span class="username">'+ message.username + '</span>' +
-    '<span class="text">'+ message.text +'</span>' +
+    '<span class="username">'+ message.username +": "+ '</span>' +
+    '<span class="text">'+ message.text +'</span>' + "<br>" +
     '<span class="roomName">'+ message.roomname + '</span>'+
     '</div>');
-
 }
 
 app.addRoom = function(roomName){
@@ -56,25 +74,32 @@ app.addRoom = function(roomName){
 }
 
 app.addFriend = function(){
-
+  
 }
 
-app.handleSubmit = function(){
+app.handleSubmit = function(msg){
 
+  var message = {
+    "username": user.username + "",
+    "text": msg,
+  }
+
+  app.send(message);
 }
 
-/*
-$(".username").on("click", function(){
+$("form").submit(function(){
+  user.message = $("input.writemsg").val();
+  app.handleSubmit(user.message)
 
-});
 
+})
 
-$("#refresh").on("click", function(event){
-  app.clearMessages();
-  app.fetch();
-
+$("form#addRoom").click(function(event){
+  var roomName = $('input.room').val();
+  app.addRoom(roomName);
   event.preventDefault();
 })
-*/
+
+
 
 
